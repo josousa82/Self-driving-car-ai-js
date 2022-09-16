471f178f0f388d2af9938b99e1ed591336de2f33
josousa82 <mfgas2@ou.ac.uk>
2022-09-16 08:59:38 +0100

    Added artificial sensor, and first sensor rays.

--- src/car.js
+++ src/car.js
@@ -10,11 +10,13 @@ class Car {
     this.friction = 0.05;
     this.angle = 0;
 
+    this.sensor = new Sensor(this);
     this.controls = new Controls();
   }
 
   update() {
     this.#move();
+    this.sensor.update();
   }
 
   #move() {
@@ -62,5 +64,6 @@ class Car {
     ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
     ctx.restore();
+    this.sensor.draw(ctx);
   }
 }
